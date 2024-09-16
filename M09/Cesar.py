f07ecddf3013120a59a4879b1d82af1475f441ab
Rakhazzan <makhazzan@inscastellet.cat>
2024-09-16 22:04:26 +0200

    Acabado

--- M09/Cesar.py
+++ M09/Cesar.py
@@ -1,3 +1,15 @@
-user_input=  input("Escriu una paraula")
-word= str(user_input)
-chars= word.split('')
+user_input = input("Escriu una paraula\n")
+
+word = str(user_input)
+
+def xifrar(x):
+    letras = []
+    xifrar_letras = []
+    for letra in x:
+        letras.append(ord(letra) + 1)
+        xifrar_letras.append(chr(ord(letra) + 1))
+    
+    print("Code points:", letras)
+    print("Encrypted letters:", ''.join(xifrar_letras))
+
+xifrar(word)
