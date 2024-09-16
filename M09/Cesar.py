df732db4c474e07a9682fa10111982a7aec5d5d0
Rakhazzan <makhazzan@inscastellet.cat>
2024-09-16 22:40:02 +0200

    Version mejorada

--- M09/Cesar.py
+++ M09/Cesar.py
@@ -1,3 +1,4 @@
+print (ord('z'))
 user_input = input("Opcions\n 1.- Xifrar \n 2.- Desxifrar\n")
 option = int(user_input)
 
@@ -5,8 +6,12 @@ def xifrar(x):
     letras = []
     xifrar_letras = []
     for letra in x:
-        letras.append(ord(letra)+1)
-        xifrar_letras.append(chr(ord(letra)+1))
+        if ord(letra)==122 :
+             letras.append(ord(letra)-25)
+             xifrar_letras.append(chr(ord(letra)-25))
+        else:
+            letras.append(ord(letra)+1)
+            xifrar_letras.append(chr(ord(letra)+1))
     
     print("Numeros ASCII:", letras)
     print("Paraula encriptada:", ''.join(xifrar_letras))
@@ -16,8 +21,12 @@ def desxifrar(x):
     letras = []
     desxifrar_letras = []
     for letra in x:
-        letras.append(ord(letra)-1)
-        desxifrar_letras.append(chr(ord(letra)-1))
+         if ord(letra)==97 :
+             letras.append(ord(letra)+25)
+             desxifrar_letras.append(chr(ord(letra)+25))
+         else:
+            letras.append(ord(letra)-1)
+            desxifrar_letras.append(chr(ord(letra)-1))
     
     print("Numeros ASCII:", letras)
     print("Paraula desencriptada:", ''.join(desxifrar_letras))
