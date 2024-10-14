ad609d80b22e07b8641a6ffa6c41e5e585821aca
Rakhazzan <makhazzan@inscastellet.cat>
2024-10-14 19:39:35 +0200

    Tasca.-6✅

--- M07/HTML/index.js
+++ M07/HTML/index.js
@@ -1,3 +1,18 @@
-function funcio1(){
-    alert ("Hem fet click sobre la imatge");
+function funcioBurguer() {
+    var menu = document.getElementById("menu");
+    var burger = document.getElementById("burger");
+    burger.style.display = "none";
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+
+
+function funcioOption() {
+    if(screen.width<768){
+        burger.style.display = "block";
+        menu.style.display = "none";
+    }
 }
