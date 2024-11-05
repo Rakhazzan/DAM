a2687f004d4ff92354337bcefe6e455ad0d25eac
Rakhazzan <makhazzan@inscastellet.cat>
2024-11-05 17:34:53 +0100

    .

--- M07/UF1/NF2/Projecte/vuep1/src/App.vue
+++ M07/UF1/NF2/Projecte/vuep1/src/App.vue
@@ -1,11 +1,39 @@
-<script setup>
-let nom = 'Mohamed'
-</script>
-
 <template>
-  <main>
-    <h1>HOLA {{ nom }}</h1>
-  </main>
+  <div class="date-time-container">
+    <p>{{ currentDate }}</p>
+    <p>{{ currentTime }}</p>
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+export default {
+  data() {
+    return {
+      currentDate: '',
+      currentTime: '',
+    }
+  },
+  created() {
+    this.updateDateTime()
+    setInterval(this.updateDateTime, 1000)
+  },
+  methods: {
+    updateDateTime() {
+      const now = new Date()
+      this.currentDate = `${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()}`
+      this.currentTime = now.toLocaleTimeString()
+    },
+  },
+}
+</script>
+
+<style scoped>
+.date-time-container {
+  background-color: black;
+  color: white;
+  text-align: center;
+  padding: 20px;
+  font-family: Arial, sans-serif;
+  font-size: 2rem;
+}
+</style>
